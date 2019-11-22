924974a2bb6abee5c8ee5409cfa20e770684bdf4
Imaraika <inange2013@gmail.com>
2019-11-22 12:55:04 +0200

    webview have been added

---
+++ app/src/main/java/com/example/weplayapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.weplayapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webb = (WebView)findViewById(R.id.wb1);
+        webb.setWebViewClient(new WebViewClient());
+        webb.loadUrl("https://imaraika.github.io/Portfolio/");
+    }
+}
